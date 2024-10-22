892fb2fcae6e050ec96a0c71d569c238c4a3512e
refatahmed441 <refatahmed441@gmail.com>
2024-10-22 14:00:54 +0600

    add aption added

--- src/Components/Router & State Management/CardOperationTypeForm/Aunty/Aunty.jsx
+++ src/Components/Router & State Management/CardOperationTypeForm/Aunty/Aunty.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import Cousin from "../Cousin/Cousin";
+import { MoneyContext } from "../Grandpa/Grandpa";
 
 const Aunty = () => {
+  const [money, setmoney] = useContext(MoneyContext);
   return (
     <div>
       <h2>Aunty</h2>
@@ -8,6 +11,8 @@ const Aunty = () => {
         <Cousin name={"promi"}></Cousin>
         <Cousin name={"panna"}></Cousin>
       </section>
+      <p>Money : {money}</p>
+      <button onClick={() => setmoney(money + 1000)}>Add 100 bdt</button>
     </div>
   );
 };
--- src/Components/Router & State Management/CardOperationTypeForm/Uncle/Uncle.jsx
+++ src/Components/Router & State Management/CardOperationTypeForm/Uncle/Uncle.jsx
@@ -1,11 +1,11 @@
 import Cousin from "../Cousin/Cousin";
 
-const Uncle = () => {
+const Uncle = ({ asset }) => {
   return (
     <div>
       <h2>Uncle</h2>
       <section className="flex">
-        <Cousin name={"Refat"}></Cousin>
+        <Cousin name={"Refat"} asset={asset}></Cousin>
         <Cousin name={"Repon"}></Cousin>
       </section>
     </div>
